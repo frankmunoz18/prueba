6606fac126f88d3a633a69f99ed6368bb25f2f49
duarte1 <duarte1@duarte7-PC>
2017-08-13 21:20:29 -0500

    Creacion Prueba con github

---
+++ prueba/src/prueba/Prueba.java
@@ -0,0 +1,14 @@
+
+package prueba;
+
+
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
