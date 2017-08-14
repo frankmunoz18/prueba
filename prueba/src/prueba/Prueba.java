1977e15fef215a84d7625b67deb34c47ed7ed94b
duarte1 <duarte1@duarte7-PC>
2017-08-13 21:26:44 -0500

    Configuracion 1

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -8,7 +8,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      System.out.print("Hola profe ya estoy en github");
     }
     
 }
